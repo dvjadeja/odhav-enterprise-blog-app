bb6ef35b5276913c4e8cc021e8cd4195856f7343
dvjadeja <dilipsinhjadeja26@gmail.com>
2025-12-07 12:07:40 +0530

    feat: :sparkles: Updated Layout and fetched Articles on the Home page
    Updated Layout and fetched Articles on the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import { Button } from "@/components/ui/button";
+import { getArticles } from "@/lib/actions/article";
 
-export default function Home() {
+export default async function ArticlesListingPage() {
+  const articles = await getArticles();
+  console.log(articles);
   return (
     <div>
       <Button>Click me</Button>
