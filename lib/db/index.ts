c4e198904a619de093118db38910fb02651c768b
dvjadeja <dilipsinhjadeja26@gmail.com>
2025-12-07 11:23:42 +0530

    feat: :sparkles: Added global index export file
    Added global index export file

---
+++ lib/db/index.ts
@@ -0,0 +1,8 @@
+export { default as connectDB } from './mongodb';
+export { default as Article } from '../models/Article';
+export type { IArticle } from '../models/Article';
+export { default as ProjectType } from '../models/ProjectType';
+export type { IProjectType } from '../models/ProjectType';
+export { default as Client } from '../models/Client';
+export type { IClient } from '../models/Client';
+
